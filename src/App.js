300186c3995f438534463a41cfd45aa377542076
yusuf-09054 <smohammedyusuf193@gmail.com>
2023-10-10 13:22:33 +0530

    addform

--- src/App.js
+++ src/App.js
@@ -7,22 +7,15 @@
 // }
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { Operator } from './ternaryoperator';
-import { Hook1,} from './Usestate';
-
-import{Route, Routes} from "react-router-dom";
-import { Homepage } from './Navigation';
+import { Registrationform } from './registrationfrom';
 
 export function App1()
 {
     return(
         <>
         <browerRouter>
-        <Homepage/>
-        <Routes>
-            <Route path='ternary1' exact element={<Operator/>}/>
-            <Route path='usestate1'exact element={<Hook1/>}/>
-        </Routes>
+        <Registrationform/>
+        
         </browerRouter>
         </>
     );
---
+++ src/registrationfrom.js
@@ -0,0 +1,78 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useState } from "react"
+export let Registrationform=()=>
+
+{
+    //const[getvariable,setvariable]=usestate(initialization)
+    const[myvalues,setmyvalues]=useState({
+       "stuid":0,
+       "stuname":"",
+       "studob":"", 
+    })
+
+    const track=(datas)=>
+    {
+        const{name,value}=datas.target
+        setmyvalues((getting)=>
+        {
+          return{
+           ...getting,
+           [name]:value
+          }
+        })
+    }
+
+    const register=()=>
+    {
+        alert("SUCCESSFULLY REGISTERED"+JSON.stringify(myvalues))
+    }
+    return(
+        <>
+        <form className="container text-center">
+            <div className="row justify-content-center">
+            <h1 className="text-center">REGISTRATION FORM</h1>
+                <div className="col-lg-8 col-md-10 col-sm-12">
+                    <div className="form-group">
+                        <label className="form-label">Enter your register no:</label>
+                        <input
+                        placeholder="Enter your regno"
+                        type="number"
+                        name="stuid"
+                        onChange={track}
+                        value={myvalues.stuid}
+                        className="form-contorl"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">Enter your name:</label>
+                        <input
+                        placeholder="Enter your name"
+                        type="text"
+                        name="stuname"
+                        onChange={track}
+                        value={myvalues.stuname}
+                        className="form-contorl"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">Enter your DOB:</label>
+                        <input
+                        placeholder="Enter your DOB"
+                        type="text"
+                        name="studob"
+                        onChange={track}
+                        value={myvalues.studob}
+                        className="form-contorl"
+                        />
+                    </div>
+                    <div className="row justify-content-center mt-5">
+                    <button className="btn btn-outline-success col-5 me-3"onClick={register}>REGISTER</button>
+                    <button className="btn btn-outline-danger col-5 me-3">CANCEL</button>
+
+                    </div>
+                </div>
+            </div>
+        </form>
+        </>
+    );
+}
